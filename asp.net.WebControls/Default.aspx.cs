9e6b3e528a6bbf755590d428642633ae1f1d55c3
rNdm <rndm@windowslive.com>
2014-04-04 09:54:46 +1300

    Uploading Work

--- asp.net.WebControls/Default.aspx.cs
+++ asp.net.WebControls/Default.aspx.cs
@@ -13,8 +13,10 @@ public partial class _Default : System.Web.UI.Page
 
     protected void Page_Load(object sender, EventArgs e)
     {
+        // Need the lists every page submit
         populateLists();
 
+        // Only fill list once
         if (!IsPostBack)
             populateDropDown();
     }
@@ -68,13 +70,19 @@ public partial class _Default : System.Web.UI.Page
 
     protected void bDisplay_Click(object sender, EventArgs e)
     {
+        // Clearing old content
         tblDisplay.Controls.Clear();
 
+        // Setup
         int stringLength = tbUserText.Text.Length;
 
+        // Assumes 2 rows of images 
+        // this will only work when a string of even length is input
+        // this is not good programming !!
         int nRows = 2;
         int nCols = stringLength / nRows;
 
+        // Display pictures
         buildPictureString(nRows, nCols);
 
     }
@@ -82,21 +90,30 @@ public partial class _Default : System.Web.UI.Page
     {
         for (int r = 0; r < nRows; r++)
         {
+            // Create a new row and add it to the table
             TableRow newRow = new TableRow();
             tblDisplay.Controls.Add(newRow);
 
+            // Populate the row with cells
             for (int c = 0; c < nCols; c++)
             {
+                // Make new cell
                 TableCell newCell = new TableCell();
-                
+
+                // Make filepath string, (r * nCols) + c flattens the 2d array back to one dimension
                 String imageFileName = "alphabet/" + ddlFont.SelectedValue + "/" + filePrefixes[ddlFont.SelectedIndex] + tbUserText.Text[(r * nCols) + c] + fileSuffixes[ddlFont.SelectedIndex] + ".gif";
+                
+                // Make the image
                 Image charImage = new Image();
                 charImage.ImageUrl = imageFileName;
 
+                // Add charImage to cell
                 newCell.Controls.Add(charImage);
 
+                // Add finished cell to the row
                 newRow.Controls.Add(newCell);
-            }
-        }
-    }
+
+            } // end cells / columns 
+        } // end rows
+    } 
 }
