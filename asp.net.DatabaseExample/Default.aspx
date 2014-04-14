ad696b17719a92ee156b96485de0ea2e473c9078
rNdm <rndm@windowslive.com>
2014-04-15 11:35:16 +1200

    Uploading Work

---
+++ asp.net.DatabaseExample/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Language="C#" AutoEventWireup="true"
+    CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Default Site</title>
+</head>
+<body>
+    
+    <form id="form1" runat="server">
+    <asp:ListBox ID="lbResult" runat="server" AutoPostBack="True" Height="188px" 
+        Width="270px"></asp:ListBox>
+    <p>
+        <asp:Button ID="bFetchData" runat="server" onclick="bFetchData_Click" 
+            Text="Fetch Data" />
+    </p>
+    </form>
+    
+</body>
+</html>
---
+++ asp.net.DatabaseExample/Default.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void bFetchData_Click(object sender, EventArgs e)
+    {
+        SqlConnection bitdevConnection = new SqlConnection();
+
+        bitdevConnection.ConnectionString = "Data Source = bitdev.ict.op.ac.nz;" +
+								            "Initial Catalog = World;" +
+								            "User ID = CHARLAL1;" +
+								            "Password = ACh_5A80;";
+        bitdevConnection.Open();
+
+        String selectQuery = "SELECT Name FROM dbo.country";
+
+        // Create the SQLCommand object, and bind it to the current db connection
+        SqlCommand worldCommand = new SqlCommand();
+        worldCommand.Connection = bitdevConnection;
+
+        // Assign the query
+        worldCommand.CommandText = selectQuery;
+
+        SqlDataReader worldReader;
+        worldReader = worldCommand.ExecuteReader();
+
+        while(worldReader.Read())
+        {
+	        lbResult.Items.Add(worldReader["Name"].ToString());
+        }
+
+        worldReader.Close();
+        bitdevConnection.Close();
+
+        
+    }
+}
