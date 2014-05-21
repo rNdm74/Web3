a28103b1374ad10a65e638937a26b4df95f0f872
rNdm <rndm@windowslive.com>
2014-05-21 19:04:57 +1200

    Uploading Work

--- asp.net.mvc.charlal1.MelbourneCupOfficeSweepstakes/asp.net.mvc.charlal1.MelbourneCupOfficeSweepstakes/Controllers/BetController.cs
+++ asp.net.mvc.charlal1.MelbourneCupOfficeSweepstakes/asp.net.mvc.charlal1.MelbourneCupOfficeSweepstakes/Controllers/BetController.cs
@@ -35,30 +35,6 @@ namespace asp.net.mvc.charlal1.MelbourneCupOfficeSweepstakes.Controllers
 
         public ActionResult Create(Player player)
         {
-            var Horses = new List<Horse> 
-            {
-                new Horse{Form="13X66X10X1", Name="GLASS HARMONIUM",    FinishingPlace=4},
-                new Horse{Form="12X3X12X91", Name="DUNADEN",            FinishingPlace=1},
-                new Horse{Form="X446X13137", Name="DRUNKEN SAILOR",     FinishingPlace=12},
-                new Horse{Form="2573X34844", Name="MANIGHAR",           FinishingPlace=5},
-                new Horse{Form="3621X5X406", Name="UNUSUAL SUSPECT",    FinishingPlace=9},
-                new Horse{Form="2164112461", Name="FOX HUNT",           FinishingPlace=7},
-                new Horse{Form="111X4215X5", Name="LUCAS CRANACH",      FinishingPlace=3},
-                new Horse{Form="X025X67009", Name="PRECEDENCE",         FinishingPlace=11},
-                new Horse{Form="9X20141053", Name="RED CADEAUX",        FinishingPlace=2},
-                new Horse{Form="189X223274", Name="LOST IN THE MOMENT", FinishingPlace=6},
-                new Horse{Form="23245X0622", Name="AT FIRST SIGHT",     FinishingPlace=10},
-                new Horse{Form="0121X07201", Name="NIWOT",              FinishingPlace=8},
-            };
-
-            foreach (Horse h in Horses)
-            {
-                //db.Horses.Add(h);
-            }
-
-            ViewBag.test = player.Name;
-            //db.SaveChanges();
-
             return View();
         } 
 
@@ -68,22 +44,6 @@ namespace asp.net.mvc.charlal1.MelbourneCupOfficeSweepstakes.Controllers
         [HttpPost]
         public ActionResult Create(Bet bet, Player player)
         {
-            Random rGen = new Random();
-
-            bet.Horses = new List<Horse>();
-
-            List<Horse> horseList = db.Horses.ToList();
-                        
-            for (int i = 0; i < bet.NoHorses; i++)
-			{
-                int randomNumber = rGen.Next(horseList.Count);
-
-                Horse pickedHorse = horseList[randomNumber];
-
-                bet.Horses.Add(pickedHorse);			 
-			}
-            
-
             if (ModelState.IsValid)
             {
                 db.Bets.Add(bet);
--- asp.net.mvc.charlal1.MelbourneCupOfficeSweepstakes/asp.net.mvc.charlal1.MelbourneCupOfficeSweepstakes/Controllers/HomeController.cs
+++ asp.net.mvc.charlal1.MelbourneCupOfficeSweepstakes/asp.net.mvc.charlal1.MelbourneCupOfficeSweepstakes/Controllers/HomeController.cs
@@ -16,6 +16,7 @@ namespace asp.net.mvc.charlal1.MelbourneCupOfficeSweepstakes.Controllers
 
         public ActionResult Index()
         {
+            //SeedDatabase();
             return View();
         }
 
@@ -23,11 +24,60 @@ namespace asp.net.mvc.charlal1.MelbourneCupOfficeSweepstakes.Controllers
         // POST: /Player/Create
         public ActionResult Result(Bet bet, Player player)
         {
-            ViewBag.Race = player.Name;
 
-            return View();
+            ViewBag.Bet = createBet(bet);
+
+            return View(createBet(bet));
         }
-        
 
+        private Bet createBet(Bet bet) 
+        {
+            Bet newBet = new Bet { BetID = bet.BetID, Name = bet.Name, NoHorses = bet.NoHorses, BetAmount = bet.BetAmount };
+
+            Random rGen = new Random();
+
+            List<Horse> pickedHorses = new List<Horse>();
+
+            List<Horse> horseList = db.Horses.ToList();
+
+            for (int i = 0; i < newBet.NoHorses; i++)
+            {
+                int randomNumber = rGen.Next(horseList.Count);
+
+                Horse pickedHorse = horseList[randomNumber];
+
+                pickedHorses.Add(pickedHorse);
+            }
+
+            newBet.Horses = pickedHorses;
+
+            return newBet;
+        }
+
+        private void SeedDatabase() 
+        {
+            var Horses = new List<Horse> 
+            {
+                new Horse{Form="13X66X10X1", Name="GLASS HARMONIUM",    FinishingPlace=4},
+                new Horse{Form="12X3X12X91", Name="DUNADEN",            FinishingPlace=1},
+                new Horse{Form="X446X13137", Name="DRUNKEN SAILOR",     FinishingPlace=12},
+                new Horse{Form="2573X34844", Name="MANIGHAR",           FinishingPlace=5},
+                new Horse{Form="3621X5X406", Name="UNUSUAL SUSPECT",    FinishingPlace=9},
+                new Horse{Form="2164112461", Name="FOX HUNT",           FinishingPlace=7},
+                new Horse{Form="111X4215X5", Name="LUCAS CRANACH",      FinishingPlace=3},
+                new Horse{Form="X025X67009", Name="PRECEDENCE",         FinishingPlace=11},
+                new Horse{Form="9X20141053", Name="RED CADEAUX",        FinishingPlace=2},
+                new Horse{Form="189X223274", Name="LOST IN THE MOMENT", FinishingPlace=6},
+                new Horse{Form="23245X0622", Name="AT FIRST SIGHT",     FinishingPlace=10},
+                new Horse{Form="0121X07201", Name="NIWOT",              FinishingPlace=8},
+            };
+
+            foreach (Horse h in Horses)
+            {
+                db.Horses.Add(h);
+            }
+
+            db.SaveChanges();
+        }
     }
 }
