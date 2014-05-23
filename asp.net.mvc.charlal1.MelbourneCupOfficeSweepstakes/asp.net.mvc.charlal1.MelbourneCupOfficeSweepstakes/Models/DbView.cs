3227c8e9314a7c586b66f9c8c004ea0778baaff4
rNdm <rndm@windowslive.com>
2014-05-23 12:25:59 +1200

    Uploading Work

---
+++ asp.net.mvc.charlal1.MelbourneCupOfficeSweepstakes/asp.net.mvc.charlal1.MelbourneCupOfficeSweepstakes/Models/DbView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace asp.net.mvc.charlal1.MelbourneCupOfficeSweepstakes.Models
+{
+    public class DbView
+    {
+        public List<Player> AllPlayers { get; set; }
+        public List<Horse> AllHorses { get; set; }
+        public Player SignUpPlayer { get; set; }
+    }
+}
